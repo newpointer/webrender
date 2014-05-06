09d017ee3e4281c1ad6013f2ce77bcae79b94219
Alexander Yastrebov <yastrebov.alex@gmail.com>
2014-05-06 19:54:31 +0400

    Реализована обработка HTTP статусов страницы рендеринга

--- render.js
+++ render.js
@@ -4,6 +4,10 @@ var ph;
 var nconf;
 var noop = function() {};
 
+function pageLog(msg) {
+    console.log('Page log:', msg);
+};
+
 exports.init = function(cfg, cb) {
     nconf = cfg;
     cb = cb || noop;
@@ -30,97 +34,110 @@ exports.exit = function(cb) {
 }
 
 exports.render = function(data, handler) {
-    ph.createPage(function (err, page) {
-        if (nconf.get('pageLog')) {
-            page.onConsoleMessage = function(msg) {
-                console.log('Page log:', msg);
-            };
-        }
-        page.onError = function(msg, trace) {
-            var msgStack = ['ERROR: ' + msg];
+    ph.createPage(function(err, page) {
+        page.onConsoleMessage = nconf.get('pageLog') ? pageLog : noop;
 
-            if (trace && trace.length) {
-                msgStack.push('TRACE:');
-
-                trace.forEach(function(t) {
-                    msgStack.push(' -> ' + t.file + ': ' + t.line + (t.function ? ' (in function "' + t.function +'")' : ''));
-                });
-            }
-
-            console.error(msgStack.join('\n'));
-        };
-
-        var ua = data.userAgent || nconf.get('userAgent');
-        setUserAgent(page, ua);
-
-        page.open(data.url, function(err, status) {
-            if (status != 'success'){
-                handler(false, {
-                    error: 'open_fail',
-                    message: 'Не удалось открыть страницу'
-                });
-                return;
-            }
-            page.set('zoomFactor', data.zoom);
-            if (data.width && data.height) {
-                page.set('viewportSize', {
-                    width: data.width,
-                    height: data.height
-                });
-            }
-            var waitResult = false;
-            waitFor(function() {
-                page.evaluate(function(check) {
-                    // Специальная проверка на строковое значение
-                    if (check && check !== 'undefined') {
-                        return this[check] === true;
-                    } else {
-                        return (document.readyState === 'complete');
-                    }
-                }, function(err, r) {
-                    waitResult = r;
-                }, data.check);
-                return waitResult;
-            }, function(ready) {
-                if (ready) {
-                    var doRender = function() {
-                        page.renderBase64('png', function(err, data) {
-                            handler(true, data);
-                        });
-                    }
-                    if (data.delay) {
-                        setTimeout(doRender, data.delay);
-                    } else {
-                        doRender();
-                    }
-                } else {
-                    handler(false, {
-                        error: 'timeout',
-                        message: 'Превышен лимит ожидания'
-                    });
-                }
-            }, data.timeout);
-        });
+        new PageHandler(page, data, handler).open();
     });
 }
 
-function waitFor(testFx, onReady, timeOutMillis) {
-    var maxtimeOutMillis = timeOutMillis ? timeOutMillis : 3000,
-    start = new Date().getTime(),
-    condition = false,
-    interval = setInterval(function() {
-        var now = new Date().getTime();
-        if ( ((now - start) < maxtimeOutMillis) && !condition ) {
-            condition = testFx()
-        } else {
-            onReady(condition);
-            clearInterval(interval);
-        }
-    }, 250);
-}
+function PageHandler(page, data, handler) {
+    var status, statusText;
 
-function setUserAgent(page, ua) {
+    page.onError = function(msg, trace) {
+        var msgStack = ['ERROR: ' + msg];
+        if (trace && trace.length) {
+            msgStack.push('TRACE:');
+
+            trace.forEach(function(t) {
+                msgStack.push(' -> ' + t.file + ': ' + t.line + (t.function ? ' (in function "' + t.function +'")' : ''));
+            });
+        }
+        console.error(msgStack.join('\n'));
+    };
+
+    // https://github.com/ariya/phantomjs/issues/10185
+    page.onResourceReceived = function(response) {
+        // NB: Точное совпадение URL. Может поломаться из-за небольших различий типа слеша в конце
+        if (response.status !== null && response.stage === 'end' && response.url === data.url) {
+            status = response.status;
+            statusText = response.statusText;
+        }
+    };
+
     // https://github.com/alexscheelmeyer/node-phantom/issues/83
     // Хак! Не поддерживает вложенные свойства, но есть eval
-    page.setFn('EVIL_EVAL', 'page.settings.userAgent = "' + ua + '"');
+    page.setFn('EVIL_EVAL', 'page.settings.userAgent = "' + (data.userAgent || nconf.get('userAgent')) + '"');
+
+    function waitFor(testFx, onReady, timeOutMillis) {
+        var maxtimeOutMillis = timeOutMillis ? timeOutMillis : 3000,
+        start = new Date().getTime(),
+        condition = false,
+        interval = setInterval(function() {
+            var now = new Date().getTime();
+            if ( ((now - start) < maxtimeOutMillis) && !condition ) {
+                condition = testFx()
+            } else {
+                onReady(condition);
+                clearInterval(interval);
+            }
+        }, 250);
+    };
+
+    function handleOpen(err, st) {
+        if (st !== 'success' || status !== 200) {
+            handler(false, {
+                error: 'open_fail',
+                message: 'Не удалось открыть страницу',
+                status: status,
+                statusText: statusText
+            });
+            return;
+        }
+        page.set('zoomFactor', data.zoom);
+        if (data.width && data.height) {
+            page.set('viewportSize', {
+                width: data.width,
+                height: data.height
+            });
+        }
+        var waitResult = false;
+        waitFor(function() {
+            page.evaluate(function(check) {
+                // Специальная проверка на строковое значение
+                if (check && check !== 'undefined') {
+                    return this[check] === true;
+                } else {
+                    return (document.readyState === 'complete');
+                }
+            }, function(err, r) {
+                waitResult = r;
+            }, data.check);
+            return waitResult;
+        }, function(ready) {
+            if (ready) {
+                var doRender = function() {
+                    page.renderBase64('png', function(err, data) {
+                        handler(true, data);
+                    });
+                }
+                if (data.delay) {
+                    setTimeout(doRender, data.delay);
+                } else {
+                    doRender();
+                }
+            } else {
+                handler(false, {
+                    error: 'timeout',
+                    message: 'Превышен лимит ожидания'
+                });
+            }
+        }, data.timeout);
+    };
+
+    return {
+        open: function() {
+            page.open(data.url, handleOpen);
+        }
+    };
 }
